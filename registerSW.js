b98723f46aa0447b12b08fecf1d7ab1ec4f02c9a
vivekevince <vivekdevince@gmail.com>
2025-02-10 15:21:04 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-react-pwa/sw.js', { scope: '/vite-react-pwa/' })})}
